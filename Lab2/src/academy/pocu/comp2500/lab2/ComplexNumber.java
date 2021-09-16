ed6d69b36654a3ccd67606068e17d0be8d26a8db
wonsukheo8 <wonsukheo8@gmail.com>
2021-09-15 20:25:12 -0500

    lab2 first try

---
+++ Lab2/src/academy/pocu/comp2500/lab2/ComplexNumber.java
@@ -0,0 +1,69 @@
+package academy.pocu.comp2500.lab2;
+
+public class ComplexNumber {
+    public double real;
+    public double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public ComplexNumber(double real) {
+        this(real, 0.0);
+    }
+
+    public ComplexNumber() {
+        this(0.0, 0.0);
+    }
+
+    public boolean isReal() {
+        if (this.imaginary == 0.0f && this.real != 0.0f) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isImaginary() {
+        if (this.real == 0.0f && this.imaginary != 0.0f) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public ComplexNumber getConjugate() {
+        return new ComplexNumber(this.real, -this.imaginary);
+    }
+
+    public ComplexNumber add(ComplexNumber num) {
+        double real = this.real + num.real;
+        double imaginary = this.imaginary + num.imaginary;
+
+        return new ComplexNumber(real, imaginary);
+    }
+
+    public ComplexNumber subtract(ComplexNumber num) {
+        double real = this.real - num.real;
+        double imaginary = this.imaginary - num.imaginary;
+
+        return new ComplexNumber(real, imaginary);
+    }
+
+    public ComplexNumber multiply(ComplexNumber num) {
+        double real = this.real * num.real - this.imaginary * num.imaginary;
+        double imaginary = this.real * num.imaginary + this.imaginary * num.real;
+
+        return new ComplexNumber(real, imaginary);
+    }
+
+    public ComplexNumber divide(ComplexNumber num) {
+        double denominator = (num.real * num.real) + (num.imaginary * num.imaginary);
+
+        double real = (this.real * num.real + this.imaginary * num.imaginary) / denominator;
+        double imaginary = (this.imaginary * num.real - this.real * num.imaginary) / denominator;
+
+        return new ComplexNumber(real, imaginary);
+    }
+}
