b64e4b55b7512f2247bbde49937d8d19ad7021c0
wonsukheo8 <wonsukheo8@gmail.com>
2021-10-12 17:44:45 -0500

    Combo meal fixing

--- Lab6/src/academy/pocu/comp2500/lab6/NoHeavyMeal.java
+++ Lab6/src/academy/pocu/comp2500/lab6/NoHeavyMeal.java
@@ -2,13 +2,8 @@ package academy.pocu.comp2500.lab6;
 import java.util.ArrayList;
 
 public class NoHeavyMeal extends ComboMeal {
-    private static final int PRICE = 15;
-    private static final int MAX_MAINCOURSE_COUNT = 0;
-    private static final int MAX_APPETIZER_COUNT = 2;
-    private static final int MAX_DESSERT_COUNT = 1;
-
     public NoHeavyMeal() {
-        super(PRICE, MAX_APPETIZER_COUNT, MAX_MAINCOURSE_COUNT, MAX_DESSERT_COUNT);
+        super(15, 2, 0, 1);
     }
 
     public void setAppetizers(Appetizer appetizer1, Appetizer appetizer2) {
--- Lab6/src/academy/pocu/comp2500/lab6/ThreeCourseMeal.java
+++ Lab6/src/academy/pocu/comp2500/lab6/ThreeCourseMeal.java
@@ -1,13 +1,8 @@
 package academy.pocu.comp2500.lab6;
 
 public class ThreeCourseMeal extends ComboMeal {
-    private static final int PRICE = 25;
-    private static final int MAX_APPETIZER_COUNT = 1;
-    private static final int MAX_DESSERT_COUNT = 1;
-    private static final int MAX_MAINCOURSE_COUNT = 1;
-
     public ThreeCourseMeal() {
-        super(PRICE, MAX_APPETIZER_COUNT, MAX_MAINCOURSE_COUNT, MAX_DESSERT_COUNT);
+        super(25, 1, 1, 1);
     }
 
     public void setMainCourse(MainCourse mainCourse) {
