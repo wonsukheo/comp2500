a01e59fc501ae14ca7c8bba6c763a8699d58852f
wonsukheo8 <wonsukheo8@gmail.com>
2021-10-13 18:07:03 -0500

    untrack un necessary files

--- Lab3/src/academy/pocu/comp2500/lab3/app/Program.java
+++
@@ -1,8 +0,0 @@
-package academy.pocu.comp2500.lab3.app;
-
-public class Program {
-
-    public static void main(String[] args) {
-	    // write your code here
-    }
-}
--- Lab4/src/academy/pocu/comp2500/lab4/app/Program.java
+++
@@ -1,8 +0,0 @@
-package academy.pocu.comp2500.lab4.app;
-
-public class Program {
-
-    public static void main(String[] args) {
-	    // write your code here
-    }
-}
