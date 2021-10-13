cc4247df582449ea90738574ef56371711bdf6ee
wonsukheo8 <wonsukheo8@gmail.com>
2021-10-13 02:17:35 -0500

    BannerColor rgb param type fix byte -> int

--- Assignment2/src/academy/pocu/comp2500/assignment2/BannerColor.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/BannerColor.java
@@ -3,7 +3,7 @@ package academy.pocu.comp2500.assignment2;
 public class BannerColor {
     protected int rgba;
 
-    public BannerColor(byte r, byte g, byte b) {
+    public BannerColor(int r, int g, int b) {
         rgba = (r << 24 | g << 16 | b << 8);
     }
 }
